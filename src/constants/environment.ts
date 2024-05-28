c3ba595dde34eaaf66c803f169c8401317240db4
Hindrance <oyvind.reitan3@gmail.com>
2024-05-28 14:01:39 +0200

    fix: env variables i gitactions er satt opp

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,6 +1,6 @@
-export const API_URL = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_API_URL : process.env.REACT_APP_API_URL;
-export const domain = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_DOMAIN : process.env.REACT_APP_AUTH0_DOMAIN;
-export const clientId = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_CLIENT_ID : process.env.REACT_APP_AUTH0_CLIENT_ID;
-export const redirectUri = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_CALLBACK_URL : process.env.REACT_APP_AUTH0_CALLBACK_URL;
-export const audience = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_AUDIENCE : process.env.REACT_APP_AUTH0_AUDIENCE;
-export const management_auth_token = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_MANAGEMENT_API_TOKEN : process.env.REACT_APP_AUTH0_MANAGEMENT_API_TOKEN;
+export const API_URL = import.meta.env.VITE_APP_API_URL
+export const domain = import.meta.env.VITE_APP_AUTH0_DOMAIN
+export const clientId = import.meta.env.VITE_APP_AUTH0_CLIENT_ID
+export const redirectUri = import.meta.env.VITE_APP_AUTH0_CALLBACK_URL
+export const audience = import.meta.env.VITE_APP_AUTH0_AUDIENCE
+export const management_auth_token = import.meta.env.VITE_APP_AUTH0_MANAGEMENT_API_TOKEN 
