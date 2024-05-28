805b60454014d453e0739232f913be61ff61de97
Hindrance <oyvind.reitan3@gmail.com>
2024-05-28 12:43:04 +0200

    fix: siste fix variabler env :)

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,6 +1,6 @@
 export const API_URL = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_API_URL : process.env.REACT_APP_API_URL;
-export const domain = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_DOMAIN : process.env.REACT_APP_API_URL;
-export const clientId = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_CLIENT_ID : process.env.REACT_APP_API_URL;
-export const redirectUri = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_CALLBACK_URL : process.env.REACT_APP_API_URL;
-export const audience = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_AUDIENCE : process.env.REACT_APP_API_URL;
+export const domain = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_DOMAIN : process.env.REACT_APP_AUTH0_DOMAIN;
+export const clientId = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_CLIENT_ID : process.env.REACT_APP_AUTH0_CLIENT_ID;
+export const redirectUri = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_CALLBACK_URL : process.env.REACT_APP_AUTH0_CALLBACK_URL;
+export const audience = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_AUDIENCE : process.env.REACT_APP_AUTH0_AUDIENCE;
 export const management_auth_token = process.env.NODE_ENV=="development" ? import.meta.env.VITE_APP_AUTH0_MANAGEMENT_API_TOKEN : process.env.REACT_APP_AUTH0_MANAGEMENT_API_TOKEN;
