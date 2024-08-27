ebc8f95f4630bf4f1a6760e28b029b40d0e3ec9e
Hindrance <oyvind.reitan3@gmail.com>
2024-08-27 15:17:26 +0200

    fix: removed unused import

--- src/pages/Auth/authorize.tsx
+++ src/pages/Auth/authorize.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import React, { useEffect } from 'react'
+import { useEffect } from 'react';
 import { redirectUri } from "../../constants/environment";
 
 
